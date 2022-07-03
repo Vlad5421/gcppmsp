eb3e2ddd9f0fb205d4138b01216aabf75ef2ac46
Vladislav <vladislav_ts@list.ru>
2022-07-03 23:00:30 +0600

    переделал фикстуры на 1 юзера админиа

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,7 +9,7 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixtures extends BaseFixtures  implements DependentFixtureInterface
+class UserFixtures extends BaseFixtures
 {
     private UserPasswordHasherInterface $passwordHasher;
     private $users = [
@@ -31,7 +31,7 @@ class UserFixtures extends BaseFixtures  implements DependentFixtureInterface
     {
         $this->createMany(User::class, 1, function (User $user) use ($manager) {
             $user
-                ->setEmail('admin@pgo.ru')
+                ->setEmail('admin@gpc.ru')
                 ->setFIO('Администратор')
                 ->setPassword($this->passwordHasher->hashPassword($user, '123456'))
                 ->setRoles(['ROLE_ADMIN'])
@@ -39,26 +39,26 @@ class UserFixtures extends BaseFixtures  implements DependentFixtureInterface
 //            $manager->persist(new ApiToken($user));
         });
         $i =1;
-        foreach ($this->users as $usr){
-
-            $this->create(User::class, function (User $user) use ($manager, $usr) {
-                $user
-                    ->setEmail($usr['email'])
-                    ->setFIO($usr['name'])
-                    ->setPassword($this->passwordHasher->hashPassword($user,'123456'))
-                    ->setComplect($this->getRandomReference(Complect::class))
-                ;
-    //            $manager->persist(new ApiToken($user));
-            }, $i);
-            $i += 1;
-        }
+//        foreach ($this->users as $usr){
+//
+//            $this->create(User::class, function (User $user) use ($manager, $usr) {
+//                $user
+//                    ->setEmail($usr['email'])
+//                    ->setFIO($usr['name'])
+//                    ->setPassword($this->passwordHasher->hashPassword($user,'123456'))
+//                    ->setComplect($this->getRandomReference(Complect::class))
+//                ;
+//    //            $manager->persist(new ApiToken($user));
+//            }, $i);
+//            $i += 1;
+//        }
         $manager->flush();
 
     }
-    public function getDependencies()
-    {
-        return [
-            ComplectFixtures::class,
-        ];
-    }
+//    public function getDependencies()
+//    {
+//        return [
+//            ComplectFixtures::class,
+//        ];
+//    }
 }
