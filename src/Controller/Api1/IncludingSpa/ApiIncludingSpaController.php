d2a7cce42d2a58cd2d4992489b414cd7367a8936
Vladislav <vladislav_ts@list.ru>
2023-08-27 22:39:06 +0600

    создал createcard для SPA

--- src/Controller/Api1/IncludingSpa/ApiIncludingSpaController.php
+++ src/Controller/Api1/IncludingSpa/ApiIncludingSpaController.php
@@ -7,8 +7,13 @@ use App\Entity\Collections;
 use App\Entity\Filial;
 use App\Entity\FilialService;
 use App\Entity\User;
+use App\Repository\CardRepository;
+use App\Repository\FilialRepository;
+use App\Repository\ServiceRepository;
+use App\Repository\UserRepository;
 use App\Services\CustomSerializer;
 use App\Services\UniversalGetData\SpaMaker;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,6 +57,72 @@ class ApiIncludingSpaController extends AbstractController
     {
         $data = $spaMaker->getServicesFromFilial($filial_id);
         return $this->json($serializer->serializeIt($data)) ;
+    }
+
+    // Создание записи по апи - работает
+    #[Route('/api1/spa/createcard', name: 'api1_spa_createcard', methods: "POST")]
+    public function boockingCreate
+    (Request $request,
+     CardRepository $cardRepo,
+     EntityManagerInterface $em,
+     UserRepository $specRepo,
+     FilialRepository $filRepo,
+     ServiceRepository $serRepo,
+    ): Response
+    {
+        $data = $request->query;
+
+        $filial = $filRepo->findOneBy(['id' => $data->get("filial")]);
+        $service = $serRepo->findOneBy(['id'=> $data->get("service")]);
+        $specialist = $specRepo->findOneBy(['id'=>$data->get("spec")]);
+        $date = new \DateTime($this->normalsDate($data->get("date")));
+        $cardCollection = $cardRepo->findBy(["filial"=>$data->get("filial"), "specialist"=>$data->get("spec"), "date"=>$date]);
+
+
+        $newCard = new Card();
+        $newCard
+            ->setFilial($filial)
+            ->setService($service)
+            ->setSpecialist($specialist)
+            ->setStart((integer)$data->get("time"))
+            ->setEndTime((integer)$data->get("time")+45)
+            ->setDate($date)
+        ;
+//        dd($newCard);
+        if(!$this->checkCard($cardCollection, $newCard)){
+            $em->persist($newCard);
+            $em->flush();
+
+            return new JsonResponse(["id" => $newCard->getId()], 201) ;
+        };
+
+
+
+        return new JsonResponse(["no" => "уже занято"], 200) ;
+    }
+
+    ///////
+    // Служебные методы
+    ////
+    public function normalsDate($date): string
+    {
+        $date_array = explode('.', $date);
+        return implode('-', array_reverse($date_array));
+    }
+
+    public function checkCard($cardCollection, $newCard){
+
+        $chekedCards = [];
+        foreach($cardCollection as $card){
+            /**
+             * @var Card $newCard
+             * @var Card $card
+             */
+            if (! ($newCard->getEndTime() <= $card->getStart() || $newCard->getStart() >= $card->getEndTime())) {
+                $chekedCards[] = $card;
+            }
+        }
+        return count($chekedCards) > 0;
     }
 
 
