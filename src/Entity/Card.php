51899d3a7a4cc2f1643ef0301be67fc815479a53
vladislav_ts@list.ru <vladislav_ts@list.ru>
2024-09-01 19:57:52 +0600

    Entity-Card: исправил метод __toString()

--- src/Entity/Card.php
+++ src/Entity/Card.php
@@ -12,7 +12,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 #[ORM\Entity(repositoryClass: CardRepository::class)]
 class Card
 {
-//    use Timestampable;
+    //    use Timestampable;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -56,30 +56,30 @@ class Card
         $this->visitors = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
 
-    public function getSpecialist(): ?User
+    public function getSpecialist() : ?User
     {
         return $this->specialist;
     }
 
-    public function setSpecialist(?User $specialist): self
+    public function setSpecialist(?User $specialist) : self
     {
         $this->specialist = $specialist;
 
         return $this;
     }
 
-    public function getDate(): ?\DateTimeInterface
+    public function getDate() : ?\DateTimeInterface
     {
         return $this->date;
     }
 
-    public function setDate(?\DateTimeInterface $date): self
-//    public function setDate(string $date): self
+    public function setDate(?\DateTimeInterface $date) : self
+    //    public function setDate(string $date): self
     {
         $this->date = $date;
 
@@ -89,14 +89,15 @@ class Card
     /**
      * @return Collection<int, Visitor>
      */
-    public function getVisitors(): Collection
+    public function getVisitors() : Collection
     {
         return $this->visitors;
     }
 
-    public function addVisitor(Visitor $visitor): self
+    public function addVisitor(Visitor $visitor) : self
     {
-        if (!$this->visitors->contains($visitor)) {
+        if (! $this->visitors->contains($visitor))
+        {
             $this->visitors[] = $visitor;
             $visitor->setCard($this);
         }
@@ -104,88 +105,90 @@ class Card
         return $this;
     }
 
-    public function removeVisitor(Visitor $visitor): self
+    public function removeVisitor(Visitor $visitor) : self
     {
-        if ($this->visitors->removeElement($visitor)) {
+        if ($this->visitors->removeElement($visitor))
+        {
             // set the owning side to null (unless already changed)
-            if ($visitor->getCard() === $this) {
+            if ($visitor->getCard() === $this)
+            {
                 $visitor->setCard(null);
             }
         }
 
         return $this;
     }
-    public function __toString(): string
+    public function __toString() : string
     {
-        return $this->getId();
+        return strval($this->getId());
     }
 
-    public function getFilial(): ?Filial
+    public function getFilial() : ?Filial
     {
         return $this->filial;
     }
 
-    public function setFilial(?Filial $filial): self
+    public function setFilial(?Filial $filial) : self
     {
         $this->filial = $filial;
 
         return $this;
     }
 
-    public function getService(): ?Service
+    public function getService() : ?Service
     {
         return $this->service;
     }
 
-    public function setService(?Service $service): self
+    public function setService(?Service $service) : self
     {
         $this->service = $service;
 
         return $this;
     }
 
-    public function getStart(): ?int
+    public function getStart() : ?int
     {
         return $this->start;
     }
 
-    public function setStart(int $start): self
+    public function setStart(int $start) : self
     {
         $this->start = $start;
 
         return $this;
     }
 
-    public function getEndTime(): ?int
+    public function getEndTime() : ?int
     {
         return $this->endTime;
     }
 
-    public function setEndTime(int $endTime): self
+    public function setEndTime(int $endTime) : self
     {
         $this->endTime = $endTime;
 
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt() : ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function setCreatedAt(\DateTimeInterface $createdAt) : self
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getUpdatedAt(): ?\DateTimeInterface
+    public function getUpdatedAt() : ?\DateTimeInterface
     {
         return $this->updatedAt;
     }
 
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    public function setUpdatedAt(\DateTimeInterface $updatedAt) : self
     {
         $this->updatedAt = $updatedAt;
 
